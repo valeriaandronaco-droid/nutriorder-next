a6df715a6c1e6e0798d43ccc1dc693f2c12c2c0a
valeriaandronaco-droid <valeria.andronaco@gmail.com>
2026-04-13 21:45:15 +0200

    configuro PWA e aggiorno favicon

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import withPWA from 'next-pwa'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const pwaConfig = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+})
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {},
+}
+
+export default pwaConfig(nextConfig)
